0342d230d2c1c6953b9e864d49d62b066e967b0f
Masato Ogura <techmasato@gmail.com>
2020-09-13 23:38:58 +0900

    edit entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo "TEST $1"
+git clone https://github.com/masato0129/learn_githubactions.git
+#cd learn_githubactions
+platformio run
 time=$(date)
 echo "::set-output name=time::$time"
-echo "11111"
-git clone https://github.com/masato0129/learn_githubactions.git
-echo "22222"
-cd learn_githubactions
-echo "33333"
-platformio run
-echo "44444"
