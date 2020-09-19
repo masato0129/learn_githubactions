21298347377eda36a0a884f9caeeed51c1c528ef
masato0129 <techmasato@gmail.com>
2020-09-20 02:40:43 +0900

    wip

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-
-echo "Hello $1"
-git clone https://github.com/masato0129/learn_githubactions.git
-cd learn_githubactions
-platformio run
-time=$(date)
-echo "::set-output name=time::$time"
