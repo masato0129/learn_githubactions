f79e02dada233710716e16fb7c0a87157ccc08db
masato0129 <techmasato@gmail.com>
2020-09-20 06:24:02 +0900

    wip

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux
-wippio lib --global install "arduino-libraries/Servo"
+pio lib --global install "arduino-libraries/Servo"
 export PATH=$PATH:/root/.platformio/penv/bin
 platformio run
