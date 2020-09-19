99122fe2a151f579768353fd001e0d3eed5e518c
masato0129 <techmasato@gmail.com>
2020-09-20 03:54:09 +0900

    wip

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,5 +3,5 @@ set -eux
 git clone https://github.com/masato0129/learn_githubactions.git
 cd learn_githubactions
 
-ENV PATH $PATH:~/.platformio/penv/bin
+export PATH $PATH:~/.platformio/penv/bin
 platformio run
